74b2e043896792d8bb66257f3cc0e0b60b1bceba
超级无敌小恐龙 <fanjianqi555@126.com>
2025-06-25 17:24:01 +0800

    rm some cached files

--- scripts/make-executable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Make the start script executable
-chmod +x /Users/franky/Documents/code/labelme-full-stack/start-frontend.sh
-echo "Script is now executable. You can run it with: ./start-frontend.sh"
